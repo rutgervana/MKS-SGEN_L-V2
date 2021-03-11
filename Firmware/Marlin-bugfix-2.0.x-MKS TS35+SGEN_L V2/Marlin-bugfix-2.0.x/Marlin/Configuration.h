a04733379a37f77a47c1ddd7249a38a1cdd51e1a
Anrooij, R. van <r.v.anrooij@student.tue.nl>
2021-03-11 16:42:46 +0100

    Set correct axis direction and set correct sensorless homing sensitivity values

--- Firmware/Marlin-bugfix-2.0.x-MKS TS35+SGEN_L V2/Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Firmware/Marlin-bugfix-2.0.x-MKS TS35+SGEN_L V2/Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -416,7 +416,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -424,7 +424,7 @@
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_6 0
 #define TEMP_SENSOR_7 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 5
 #define TEMP_SENSOR_PROBE 0
 #define TEMP_SENSOR_CHAMBER 0
 
@@ -1094,9 +1094,9 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR true
+#define INVERT_X_DIR false
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
